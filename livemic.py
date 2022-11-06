2f6fc6a378d1919a0e4081aa38773f6037a6f7c0
John Zolton <102374100+JohnZolton@users.noreply.github.com>
2022-11-05 20:14:39 -0400

    Add files via upload

--- livemic.py
+++ livemic.py
@@ -11,38 +11,59 @@ from scipy.io.wavfile import write
 import wavio as wv
 import whisper
 import timeit
-
-model = whisper.load_model("base")
-
-# Sampling frequency
-freq = 44100
-
-# Recording duration
-duration = 5
-print('Recording')
-text = ""
-while True:
-# Start recorder with the given values
-# of duration and sample frequency
-    recording = sd.rec(int(duration * freq),
-                    samplerate=freq, channels=2)
-
-    # Record audio for the given number of seconds
-    sd.wait()
-    # i wonder if i can bounce between two files so i don't lose words
-
-    # Convert the NumPy array to audio file
-    wv.write("recording0.wav", recording, freq, sampwidth=2)
+import multiprocessing
 
 
-    audio = whisper.load_audio("recording0.wav")
-    audio = whisper.pad_or_trim(audio)
+
+def record(conn):
+    # Sampling frequency
+    freq = 44100
+    # Recording duration
+    duration = 5
+    print('Recording')
     
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
-    options = whisper.DecodingOptions(language= 'en', fp16=False)
-    result = whisper.decode(model, mel, options)
+    while True:
+        newmsg = False
+        conn.send(newmsg)
+        # Start recorder with the given values
+        # of duration and sample frequency
+        recording = sd.rec(int(duration * freq),
+                        samplerate=freq, channels=2)
 
-    text += result.text
-    print(result.text)
-    print()
-# i need to run the recording and the transcribing in parallel so i don't lose words
+        # Record audio for the given number of seconds
+        sd.wait()
+        # i wonder if i can bounce between two files so i don't lose words
+
+        # Convert the NumPy array to audio file
+        wv.write("recording0.wav", recording, freq, sampwidth=2)
+        newmsg = True
+        conn.send(newmsg)
+
+def transcribe(conn, model):
+
+    while True:
+        while True:
+            newmsg = conn.recv()
+            if newmsg:
+                break
+        
+        audio = whisper.load_audio("recording0.wav")
+        audio = whisper.pad_or_trim(audio)
+        
+        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+        options = whisper.DecodingOptions(language= 'en', fp16=False)
+        result = whisper.decode(model, mel, options)
+
+        print(result.text)
+        print()
+
+if __name__=="__main__":
+    model = whisper.load_model("tiny")
+
+    to_mic, to_whisper = multiprocessing.Pipe()
+    mic = multiprocessing.Process(target=record, args = (to_whisper,))
+    write = multiprocessing.Process(target=transcribe, args = (to_mic, model))
+    mic.start()
+    write.start()
+    mic.join()
+    write.join()
