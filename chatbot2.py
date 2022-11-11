67c5b5811109444ed6b267b2be4c4df370c4c7b8
John Zolton <102374100+JohnZolton@users.noreply.github.com>
2022-11-10 20:32:52 -0500

    Add files via upload

--- chatbot2.py
+++ chatbot2.py
@@ -5,8 +5,8 @@ import sounddevice as sd
 import wavio as wv
 import whisper
 import timeit
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
+from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
+
 
 
 def record():
@@ -28,23 +28,22 @@ def transcribe(model):
     return result['text']
 
 whisper_model = whisper.load_model("small.en")
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
-model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
+
+#download and setup the model and tokenizer
+model_name = 'facebook/blenderbot-400M-distill'
+tokenizer = BlenderbotTokenizer.from_pretrained(model_name)
+model = BlenderbotForConditionalGeneration.from_pretrained(model_name)
 
 
-# Let's chat for 5 lines
-for step in range(5):
+for i in range(5):
+    print()
+while True:
     message = record()
     message = transcribe(whisper_model)
-    print('Heard: ', message)
-    # encode the new user input, add the eos_token and return a tensor in Pytorch
-    new_user_input_ids = tokenizer.encode(message + tokenizer.eos_token, return_tensors='pt')
+    print('USER: ', message)
 
-    # append the new user input tokens to the chat history
-    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-
-    # generated a response while limiting the total chat history to 1000 tokens, 
-    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-    # pretty print last ouput tokens from bot
-    print("DialoGPT: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+    #tokenize the utterance
+    inputs = tokenizer(message, return_tensors="pt")
+    result = model.generate(**inputs, max_new_tokens=1000)
+    response = tokenizer.decode(result[0])
+    print('CHATBOT: ', response[3:-4])
